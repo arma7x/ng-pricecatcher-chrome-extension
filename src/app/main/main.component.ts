baa37320c359f6dae0435c8f37514986f0452436
Ahmad Malik <arma7x@live.com>
2023-02-26 18:05:58 +0800

    extract db data

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,6 +9,10 @@ import { DatabaseService } from '../database.service';
 })
 export class MainComponent implements OnInit {
 
+  itemGroup: Array<string> = [];
+  itemCategory: Array<string> = [];
+  statesTree: { [key: string]: { [key: string]: Array<string>; }; } = {};
+
   constructor(private router: Router, private database: DatabaseService) { }
 
   ngOnInit(): void {
@@ -22,8 +26,25 @@ export class MainComponent implements OnInit {
   public async getItems() {
     try {
       const db = await this.database.instance.init();
-      const items = db.exec("SELECT * from items;");
-      console.log(items);
+
+      const item_group = db.exec("SELECT item_group FROM items WHERE NOT item_code=-1 GROUP BY item_group;");
+      this.itemGroup = item_group[0].values.flat();
+
+      const item_category = db.exec("SELECT item_category FROM items WHERE NOT item_code=-1 GROUP BY item_category;");
+      this.itemCategory = item_category[0].values.flat();
+
+      const states = db.exec("SELECT state, district, premise_type FROM premises WHERE NOT premise_code=-1 GROUP BY state, district, premise_type ORDER BY state ASC, district ASC, premise_type ASC;");
+      for (let key in states[0].values) {
+        let [state, district, type] = states[0].values[key];
+        if (this.statesTree[state] == null)
+          this.statesTree[state] = {};
+        if (this.statesTree[state][district] == null)
+          this.statesTree[state][district] = [];
+        if (this.statesTree[state][district].indexOf(type) < 0)
+          this.statesTree[state][district].push(type);
+      }
+      console.log(this.itemGroup, this.itemCategory, this.statesTree);
+
     } catch (err) {
       console.log(err);
     }
